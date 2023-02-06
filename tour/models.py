a409edbc555a74efe3242fa4775e90690bba5448
beluywolk <alankoshmuratov42@gmail.com>
2023-02-06 22:54:39 +0600

    sprint-createmodels-2 создал модельки работал в tour.models и user.models

--- tour/models.py
+++ tour/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from user.models import CustomUser
 
-# Create your models here.
+DURATION_CHOICES= (
+    ('1', 'День'),
+    ('3', '3 дня'),
+    ('7','Неделя'),
+)
+COMPLEXITY_CHOICES = (
+    ('Easy', 'Легкий'),
+    ('Meduim', 'Средний'),
+    ('Hard','Сложный'),
+    ('Extra','Экстра-сложный')
+)
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    image = models.ImageField()
+    slug = models.SlugField()
+
+class Place(models.Model):
+    name_city = models.CharField()
+    name_place = models.CharField(null=True)
+
+
+
+class Tour(models.Model):
+    title = models.CharField(max_lenght=25)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, null=True)
+    slug = models.SlugField()
+    date_publishied = models.DateField()
+    date_departure = models.DateTimeField()
+    date_arrival = models.DateTimeField()
+    from_ = models.ForeignKey(Place)
+    to = models.ForeignKey(Place)
+    quantity_limit = models.PositiveIntegerField()
+    actual_limit = models.PositiveIntegerField()
+    is_hot = models.BooleanField(null=True, default=False)
+    duration_field = models.CharField(choices=DURATION_CHOICES)
+    complexity = models.CharField(choices=COMPLEXITY_CHOICES)
+
+class Review(models.Model):
+    text = models.TextField
+    post = models.ForeignKey(Tour)
+    autor = models.OneToOneField(CustomUser)
+
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Avatar(models.Model):
+    image = models.ImageField()
+
+
+
+class CustomUser(models.User):
+    avatar = models.ForeignKey(Avatar)
