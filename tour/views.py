c8a020ec20b2432a5b941e1a005e4296e6d003e2
ilim05 <ilimzutanov4@gmail.com>
2023-02-18 18:56:55 +0600

    добавил view для отправки и и прсмотра отзывов

--- tour/views.py
+++ tour/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from tour.models import Review, Tour
+from rest_framework.views import APIView
+class tour_reviews(APIView):
+    def get(self, request):
+        tour = Tour.objects.get(id=id)
+        reviews = Review.objects.filter(tour_id=id)
+        return Response({
+            'tour': tour,
+            'categories': tour.categories.name,
+            'reviews': reviews,
+        })
+    def post(self, request):
+        tour = Tour.objects.get(id=id)
+        review_new = Review.objects.create(
+            text=request.data['text'],
 
-# Create your views here.
+        )
+        return Response({
+            'tour': tour,
+            'categories': tour.categories.name,
+            'reviews': model_to_dict(review_new),
+            'autor': request.user(request)
+        })
+
+
+
