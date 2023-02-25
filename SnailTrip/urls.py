3ada0dab0ae4d38bc1634b6fec95b967e1666209
OVERDOZZZE <saparbekov113@gmail.com>
2023-02-25 23:02:13 +0600

    Added django filtration, APIView changed to mixins classes, add search fields

--- SnailTrip/urls.py
+++ SnailTrip/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('tour/', include('tour.urls')),
     path('user/', include('user.urls'))
 
 ]
--- tour/views.py
+++ tour/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, mixins
+from rest_framework.viewsets import GenericViewSet
+
 from .serializers import *
+from rest_framework import filters
+from .service import TourFilter
 
 
-class TourListView(generics.ListAPIView):
+class TourViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  GenericViewSet):
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_class = TourFilter
+
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
+    search_fields = 'title'
+    lookup_field = 'slug'
