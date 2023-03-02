734c309e6d1322726f9b123d7d6abbd69ae0df5d
Elnura <elena040203@gmail.com>
2023-03-02 18:49:31 +0600

    commit

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.i18n import i18n_patterns
 from .settings.drf_yasg import urlpatterns
-
+from django.conf import settings
+from django.conf.urls.static import static
 admin.site.site_header = "Моя админка"
 admin.site.index_title = "Мои модели"
 
@@ -16,3 +17,5 @@ urlpatterns = [
     path("profiles/", include("src.profiles.urls")),
     *urlpatterns,
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
