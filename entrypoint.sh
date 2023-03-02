c351f4eee5e3e49e13a5cd8b72b0e0324c26351f
Elnura <elena040203@gmail.com>
2023-03-02 14:42:23 +0600

    fix bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --bind 0.0.0.0:8000
