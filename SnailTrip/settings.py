6bff2f04d344e7c1549a5fd304ec394e91213eaf
beluywolk <alankoshmuratov42@gmail.com>
2023-03-03 20:49:24 +0600

    добавил BASE_URL

--- SnailTrip/settings.py
+++ SnailTrip/settings.py
@@ -4,7 +4,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_URL = 'http://127.0.0.1:8000/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
